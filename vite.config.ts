c39fee48153f2d5cc0927677951a5acc926b5a6e
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-05-19 01:52:51 +0330

    configure tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
@@ -8,5 +9,6 @@ export default defineConfig({
     svgr({
       svgrOptions: { icon: true },
     }),
+    tailwindcss(),
   ],
 });
