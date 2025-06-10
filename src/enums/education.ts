50dd0acdbc7573001638ac5909d77f31fbbfb14b
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-06-10 23:40:49 +0330

    add job and education enums

---
+++ src/enums/education.ts
@@ -0,0 +1,8 @@
+export enum EducationOptions {
+  HIGH_SCHOOL = "دبیرستان",
+  DIPLOMA = "دیپلم",
+  ASSOCIATE = "فوق دیپلم",
+  LICENCE = "لیسانس",
+  MASTER = "فوق لیسانس",
+  DOCTORATE = "دکترا",
+}
---
+++ src/enums/job.ts
@@ -0,0 +1,11 @@
+export enum JobOptions {
+  NONE = "ندارد",
+  STUDENT = "دانشجو",
+  DEVELOPER = "توسعه‌دهنده",
+  DESIGNER = "طراح",
+  TESTER = "تستر",
+  MANAGER = "مدیر",
+  TEACHER = "معلم",
+  RESEARCHER = "پژوهشگر",
+  OTHER = "سایر",
+}
