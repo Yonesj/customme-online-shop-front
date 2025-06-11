28c4572b34f996630fed0fbf9bb12f259a12dc4f
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-06-11 17:38:37 +0330

    add ToastContainer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import OrdersHistory from "./pages/OrdersHistory";
@@ -7,7 +10,8 @@ import OtherProducts from "./pages/OtherProducts";
 import SelectedProduct from "./pages/SelectedProduct";
 import UserAccountInfo from "./pages/UserAccountInfo";
 import UserProfile from "./pages/UserProfile";
-import UserSignUp from "./pages/UserSignUp/UserSignUp";
+import UserSignUp from "./pages/UserSignUp/UserSignUp.tsx";
+import UserSignUpPage2 from "./pages/UserSignUp/UserSignUpPage2.tsx";
 
 import Header from "./components/Header.tsx";
 import Footer from "./components/Footer.tsx";
@@ -15,6 +19,7 @@ import Footer from "./components/Footer.tsx";
 function App() {
   return (
     <BrowserRouter>
+      <ToastContainer position="top-center" autoClose={3000} />
       <Header />
 
       <Routes>
@@ -23,8 +28,9 @@ function App() {
         <Route path="/products" element={<OtherProducts />} />
         <Route path="/products/:id" element={<SelectedProduct />} />
         <Route path="/users/sign-up" element={<UserSignUp />} />
+        <Route path="/users/sign-up/complete" element={<UserSignUpPage2 />} />
         <Route path="/my-profile/dashboard" element={<UserProfile />} />
-        <Route path="/my-profile/orderss" element={<OrdersHistory />} />
+        <Route path="/my-profile/orders" element={<OrdersHistory />} />
         <Route path="/my-profile/info" element={<UserAccountInfo />} />
 
         {/*<Route path="*" element={<NotFound />} />*/}
