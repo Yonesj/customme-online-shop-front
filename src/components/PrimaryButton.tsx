4ae1b61d2a655e241a7b77a60f54812d1e7ef203
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-06-11 21:33:24 +0330

    make PrimaryButton component more reusable by passing style

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -1,16 +1,19 @@
 import { type JSX, ReactNode } from "react";
+import clsx from "clsx";
 
 interface Props {
   text: string;
   icon?: ReactNode;
+  className?: string;
 }
 
-function PrimaryButton({ text, icon }: Props): JSX.Element {
+function PrimaryButton({ text, icon, className }: Props): JSX.Element {
   return (
     <button
-      className="
-        flex items-center justify-center gap-[0.4rem] text-[#FFF] bg-[#a72f3b]
-        py-[0.8rem] px-[2.4rem] border-none rounded-[0.8rem]"
+      className={clsx(
+        "flex items-center justify-center gap-[0.4rem] text-[#FFF] bg-[#a72f3b] py-[0.8rem] px-[2.4rem] border-none rounded-[0.8rem]",
+        className ?? "",
+      )}
     >
       {icon}
       <span className="button-2">{text}</span>
