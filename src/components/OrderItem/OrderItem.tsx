5cd64e1192cde29752d34a234737a65385100dd9
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-06-11 17:44:00 +0330

    make order item component more reusable

--- src/components/OrderItem/OrderItem.tsx
+++ src/components/OrderItem/OrderItem.tsx
@@ -2,9 +2,14 @@ import { type JSX } from "react";
 
 import "./OrderItem.css";
 import ProcessIcon from "../../assets/icons/order_history/processing_icon.svg?react";
+import DeliveredIcon from "../../assets/icons/order_history/delivered.svg?react";
+import CancelIcon from "../../assets/icons/order_history/canceled_icon.svg?react";
 import BillIcon from "../../assets/icons/order_history/bill.svg?react";
 
+// type OrderState = "pending" | "delivered" | "canceled";
+
 interface OrderItemProps {
+  state: string;
   createdAt: string;
   orderCode: string;
   totalCost: string;
@@ -12,19 +17,37 @@ interface OrderItemProps {
   orderImagePaths: string[];
 }
 
+const stateConfig: Record<string, { text: string; icon: JSX.Element }> = {
+  pending: {
+    text: "در حال پردازش",
+    icon: <ProcessIcon className="w-[2.4rem] h-[2.4rem]" />,
+  },
+  delivered: {
+    text: "تحویل شده",
+    icon: <DeliveredIcon className="w-[2.4rem] h-[2.4rem]" />,
+  },
+  canceled: {
+    text: "لغو شده",
+    icon: <CancelIcon className="w-[2.4rem] h-[2.4rem]" />,
+  },
+};
+
 function OrderItem({
+  state,
   createdAt,
   orderCode,
   totalCost,
   discount,
   orderImagePaths,
 }: OrderItemProps): JSX.Element {
+  const { text, icon } = stateConfig[state];
+
   return (
     <div className="order">
       <div className="order-info body-3">
         <div className="order-state">
-          <ProcessIcon className="w-[2.4rem] h-[2.4rem]" />
-          <span>در حال پردازش</span>
+          {icon}
+          <span>{text}</span>
         </div>
 
         <div className="order-details">
