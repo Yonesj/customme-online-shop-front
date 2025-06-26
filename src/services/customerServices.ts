5fcac6f2e7e3aebf8b1c63c976648ed228218351
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-06-26 17:19:30 +0330

    add retrieveOrderDetails method to customerService

--- src/services/customerServices.ts
+++ src/services/customerServices.ts
@@ -11,12 +11,36 @@ export interface RetrieveProfileResponse {
   orders_count: number;
 }
 
+export interface RetrieveOrderDetailsResponse {
+  current_count: number;
+  canceled_count: number;
+  completed_count: number;
+  gallery_count: number;
+  comment_count: number;
+}
+
 class CustomerService {
   retrieveProfile() {
     const controller = new AbortController();
 
     const request: AxiosPromise<RetrieveProfileResponse> = client.get(
       "profile/",
+      {
+        signal: controller.signal,
+      },
+    );
+
+    return {
+      request,
+      cancel: () => controller.abort(),
+    };
+  }
+
+  retrieveOrderDetails() {
+    const controller = new AbortController();
+
+    const request: AxiosPromise<RetrieveOrderDetailsResponse> = client.get(
+      "profile/order-details/",
       {
         signal: controller.signal,
       },
