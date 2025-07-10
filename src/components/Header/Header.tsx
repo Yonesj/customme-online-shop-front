5463a828ceb1f0cb766ecc915935aaeb1a14af58
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-07-11 02:08:32 +0330

    make header component responsive

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,34 +1,43 @@
 import { NavLink } from "react-router-dom";
 import { type JSX } from "react";
 
-import LogoIcon from "../assets/icons/header/logoImg.svg?react";
-import SearchIcon from "../assets/icons/header/search-normal.svg?react";
-import ShoppingCartIcon from "../assets/icons/header/shopping-cart.svg?react";
-import LoggingIcon from "../assets/icons/header/login.svg?react";
+import LogoIcon from "../../assets/icons/header/logoImg.svg?react";
+import SearchIcon from "../../assets/icons/header/search-normal.svg?react";
+import ShoppingCartIcon from "../../assets/icons/header/shopping-cart.svg?react";
+import LoggingIcon from "../../assets/icons/header/login.svg?react";
+import MenuIcon from "../../assets/icons/header/menu.svg?react";
+import MobileLogoIcon from "../../assets/icons/header/LOGO-Mobile.svg?react";
 
 function Header(): JSX.Element {
   return (
     <header
       className="
-        h-[10.4rem]
         flex items-center justify-between
-        gap-[3.2rem]
-        px-[10.8rem] py-[1.2rem]
-        border-b border-[#f0f0f0]
+        w-full xl:w-[144rem]
+        h-[4rem] xl:h-[10.4rem]
+        mx-[3.2rem] xl:mx-auto
+        gap-[1.6rem] xl:gap-[3.2rem]
+        py-[4.8rem] xl:py-[1.2rem]
+        xl:border-b xl:border-[#f0f0f0]
       "
     >
-      <figure>
+      <figure className="hidden xl:block">
         <NavLink to="/" end>
           <LogoIcon className="w-[18.9rem] h-auto block" />
         </NavLink>
       </figure>
 
+      <figure className="block xl:hidden">
+        <MenuIcon className="w-[2.4rem] h-[2.4rem]" />
+      </figure>
+
       <div
         className="
           flex items-center justify-between
-          w-[59.6rem] h-[4.8rem]
           border border-[#afafaf] rounded-[1.6rem]
-          px-[1.2rem] bg-[#f8f8f8]
+          bg-transparent px-[1.2rem]
+          w-full xl:w-[59.6rem]
+          h-[3.2rem] xl:h-[4.8rem]
         "
       >
         <input
@@ -37,16 +46,20 @@ function Header(): JSX.Element {
           className="
             flex-1
             body-4
-            leading-[1.8rem]
             text-[#a72f3b] text-right
-            bg-transparent border-none outline-none
+            bg-transparent border-none outline-none outline-hidden
             placeholder:text-[#a72f3b]
           "
         />
-        <SearchIcon className="w-[2.4rem] h-[2.4rem]" />
+        <SearchIcon
+          className="
+            w-[1.6rem] xl:w-[2.4rem]
+            h-[1.6rem] xl:h-[2.4rem]
+          "
+        />
       </div>
 
-      <div className="flex items-center gap-[3.2rem]">
+      <div className="hidden xl:flex items-center gap-[3.2rem]">
         <NavLink
           to="/users/login"
           className="
@@ -68,6 +81,8 @@ function Header(): JSX.Element {
           <span className="button-2">سبد خرید</span>
         </NavLink>
       </div>
+
+      <MobileLogoIcon className="w-[4rem] h-[4rem] xl:hidden" />
     </header>
   );
 }
