0609d90bdd3faa6e531f78e92d8c9ed175d9bd8c
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-07-18 23:14:35 +0330

    make Category component responsive

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -8,9 +8,22 @@ interface CategoryProps {
 
 function Category({ text, image }: CategoryProps): JSX.Element {
   return (
-    <div className="category">
-      <div className="product-img">{image}</div>
-      <p className="category-text caption-1">{text}</p>
+    <div
+      className="
+        category
+        w-[7rem] h-[7rem] p-[0.4rem] rounded-[0.8rem]
+        xl:h-[12.8rem] xl:w-auto xl:p-[0.8rem] xl:rounded-[1.6rem]
+      "
+    >
+      <div className="h-auto w-full">{image}</div>
+      <p
+        className="
+          caption-4
+          xl:!font-bold  xl:!text-[1.4rem] xl:!leading-[180%]
+        "
+      >
+        {text}
+      </p>
     </div>
   );
 }
