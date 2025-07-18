fa31023359f17c9a8fe803b04ead092feabe331b
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-07-18 19:36:02 +0330

    make DesignerProfileCard component responsive

--- src/components/DesignerProfileCard/DesignerProfileCard.tsx
+++ src/components/DesignerProfileCard/DesignerProfileCard.tsx
@@ -15,25 +15,85 @@ function DesignerProfileCard({
   showcases,
 }: DesignerProfileCardProps): JSX.Element {
   return (
-    <div className="designer-profile-card">
-      <div className="designer-profile-pic">{profileImg}</div>
+    <div
+      className="
+        designer-profile-card
+        gap-[0.8rem]
+        xl:gap-[1.6rem]
+      "
+    >
+      <div
+        className="
+          designer-profile-pic
+          h-[4.7rem] mb-[1.6rem]
+          xl:h-[7.2rem] xl:mb-[4.1rem]
+        "
+      >
+        {profileImg}
+      </div>
 
       <div className="desinger-info">
-        <span className="designer-name body-3">{name}</span>
+        <span
+          className="
+            designer-name body-3 p-[0.8rem]
+            caption-3 pb-0
+            xl:!font-normal xl:!text-[1.6rem] xl:!leading-[180%]
+          "
+        >
+          {name}
+        </span>
 
-        <div className="designer-attributes caption-4">
-          <span>دنبال کنندگان : ۴۰۰</span>
-          <div className="designer-info-vertical-separator"></div>
-          <span> تعداد طرح ها : ۵۶</span>
-          <div className="designer-info-vertical-separator"></div>
-          <span> آمار فروش : ۳۷۰ </span>
+        <div
+          className="
+            designer-attributes caption-4
+            flex-col
+            xl:flex-row
+          "
+        >
+          <span className="text-center">دنبال کنندگان : ۴۰۰</span>
+          <div
+            className="
+              designer-info-vertical-separator
+              hidden
+              xl:block
+            "
+          ></div>
+          <span className="text-center"> تعداد طرح ها : ۵۶</span>
+          <div
+            className="
+              designer-info-vertical-separator
+              hidden
+              xl:block
+            "
+          ></div>
+          <span className="text-center"> آمار فروش : ۳۷۰ </span>
         </div>
 
-        <div className="designer-show-cases">{...showcases}</div>
+        <div
+          className="
+            hidden
+            xl:flex xl:items-center xl:justify-between xl:px-[0.4rem]
+          "
+        >
+          {...showcases}
+        </div>
 
-        <button className="follow-btn">
-          <SubscribeIcon />
-          <span className="button-2">دنبال کردن</span>
+        <button
+          className="
+            follow-btn py-[0.8rem]
+            gap-[0.4rem] px-[1.2rem]
+            xl:gap-[0.8rem] xl:px-[2.4rem]
+          "
+        >
+          <SubscribeIcon className="w-[1.6rem] h-[1.6rem] xl:w-[2.4rem] xl:h-[2.4rem]" />
+          <span
+            className="
+              caption-4
+              xl:!font-medium xl:!text-[1.4rem] xl:!leading-[2.4rem]
+            "
+          >
+            دنبال کردن
+          </span>
         </button>
       </div>
     </div>
