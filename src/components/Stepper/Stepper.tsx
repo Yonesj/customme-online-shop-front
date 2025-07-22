4392de8285b7613fe5867aa34ad6fa54d6397828
Younes Jamshidi <jamshidiyounes92@gmail.com>
2025-07-22 16:27:52 +0330

    make stepper component responsive

--- src/components/Stepper/Stepper.tsx
+++ src/components/Stepper/Stepper.tsx
@@ -15,27 +15,64 @@ interface StepperProps {
 
 function Stepper({ steps, currentStepIndex }: StepperProps): JSX.Element {
   if (steps.length === 0) return;
+  const activeDeg = ((currentStepIndex + 1) / steps.length) * 360;
 
   return (
-    <div className="step-indicator">
-      {steps.map((s, index) => (
-        <Fragment key={index}>
-          <div className="stepper">
-            {s.icon}
-            <span
-              className="body-5"
-              style={{
-                color: index <= currentStepIndex ? "#a72f3b" : "#edd5d8",
-              }}
-            >
-              {s.info}
+    <>
+      <div
+        className="
+          flex items-start justify-between p-[2rem]
+          xl:hidden
+        "
+      >
+        <div>
+          <h3 className="body-3 text-[#242424]">
+            {steps[currentStepIndex].info}
+          </h3>
+          {currentStepIndex !== steps.length - 1 && (
+            <p className="body-light text-[#AFAFAF]">
+              مرحله بعد: {steps[currentStepIndex + 1].info}
+            </p>
+          )}
+        </div>
+
+        <div
+          className="outer-circle"
+          style={{
+            background: `conic-gradient(
+            #A72F3B 0deg ${activeDeg}deg,
+            #D9D9D9 ${activeDeg}deg 360deg
+          )`,
+          }}
+        >
+          <div className="inner-circle">
+            <span className="body-light text-[#242424]">
+              {currentStepIndex + 1} از {steps.length}
             </span>
           </div>
+        </div>
+      </div>
 
-          {index < steps.length - 1 && <div className="step-line" />}
-        </Fragment>
-      ))}
-    </div>
+      <div className="step-indicator hidden xl:flex">
+        {steps.map((s, index) => (
+          <Fragment key={index}>
+            <div className="stepper">
+              {s.icon}
+              <span
+                className="body-5"
+                style={{
+                  color: index <= currentStepIndex ? "#a72f3b" : "#edd5d8",
+                }}
+              >
+                {s.info}
+              </span>
+            </div>
+
+            {index < steps.length - 1 && <div className="step-line" />}
+          </Fragment>
+        ))}
+      </div>
+    </>
   );
 }
 
